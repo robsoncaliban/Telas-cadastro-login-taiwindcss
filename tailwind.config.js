5e7d862babfeca406e05b99b8f0843710b6bc955
robsoncaliban <robsoncaliban@gmail.com>
2024-03-10 14:25:32 -0300

    Tela de login criada

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{css,html,ts,js}"
+  ],
+  theme: {
+    extend: {
+      colors: {
+        "azul-escuro" : "#0F1724"
+      }
+    },
+  },
+  plugins: [],
+}
+
